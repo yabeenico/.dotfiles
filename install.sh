b161d47bcebc0a41a7bb056a0db1e02f37d74f34
yabeenico <>
2022-02-01 16:10:24 +0000

    .lesskey: add

--- install.sh
+++ install.sh
@@ -6,15 +6,13 @@ ln -sf ~/.dotfiles/.bashrc              ~/
 ln -sf ~/.dotfiles/.colorrc             ~/
 ln -sf ~/.dotfiles/.gitconfig           ~/
 ln -sf ~/.dotfiles/.inputrc             ~/
+ln -sf ~/.dotfiles/.lesskey             ~/
 ln -sf ~/.dotfiles/.screenrc            ~/
 ln -sf ~/.dotfiles/.tmux.conf           ~/
 ln -sf ~/.dotfiles/.vim                 ~/
 ln -sf ~/.dotfiles/.vimrc               ~/
 
 touch ~/.hushlogin
-
-#sudo mkdir -p /root/.vim/anydir
-#sudo cp ~/.dotfiles/.vimrc_root /root/.vimrc
 
 mkdir -p ~/.vim/anydir
 vim
