7e46e52110f9aaec9ad75166d61c4cf2142e4b21
yabeenico <>
2023-09-21 10:15:58 +0900

    sudo.sh: apt-fast debconf tools

--- sudo.sh
+++ sudo.sh
@@ -7,13 +7,18 @@ sudo cp .vimrc_root /root/.vimrc
 
 # apt-fast
 if ! which apt-fast &>/dev/null; then
-    sudo add-apt-repository -y ppa:apt-fast/stable &&
+    sudo -E add-apt-repository -y ppa:apt-fast/stable &&
+    echo '
+        debconf apt-fast/maxdownloads string 20
+        debconf apt-fast/dlflag boolean true
+        debconf apt-fast/aptmanager string apt-get
+    ' | sudo debconf-set-selections
     sudo apt-get install -y apt-fast
 fi
 
 # vim
 if ! grep -R jonathonf/vim /etc/apt/sources.list.d >/dev/null; then
-    sudo add-apt-repository -y ppa:jonathonf/vim &&
+    sudo -E add-apt-repository -y ppa:jonathonf/vim &&
     sudo apt-fast -y --only-upgrade install vim-nox
 fi
 
